8ac49f8bde8d11f4cf749994bd00e5c333d72fa1
nemoto <haikonggenben@gmail.com>
2025-09-18 10:20:12 +0900

    CSS_repair_detail
    細かくCSS修正しました。まだ、修正したいところあります

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <main>
+        <span class="sign-post"></span> <!-- 共通の棒 -->
         <a href="{{ route('posts.create') }}" class="post-button post-button_normal">通常投稿</a>
+        <span class="sign-post"></span>
+        {{-- イベント投稿ページへのリンク --}}
         <a href="{{ route('posts.post_event') }}" class="post-button post-button_event">イベント投稿</a>
     </main>
 @endsection
