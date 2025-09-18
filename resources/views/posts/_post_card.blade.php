3afc681a405549ce6a62c8ed527d8319a5567cbd
nemoto <haikonggenben@gmail.com>
2025-09-18 22:37:46 +0900

    onaoshi
    直したので確認お願いします

--- resources/views/posts/_post_card.blade.php
+++ resources/views/posts/_post_card.blade.php
@@ -12,19 +12,24 @@
     <div class="post-actions">
         <div class="actions-left" style="display: flex; align-items: center; gap: 50px;">
             <div class="like-section" id="like-section-{{ $post->id }}">
-                @if ($post->isLikedBy(Auth::user()))
+                {{-- いいねの状態に応じてフォームとアイコンを切り替え --}}
+                @if (Auth::check() && $post->isLikedBy(Auth::user()))
+                    {{-- いいね済みの場合（いいね解除フォーム） --}}
                     <form action="{{ route('posts.unlike', $post) }}" method="POST" class="like-form"
                         data-post-id="{{ $post->id }}" style="display: inline;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="like-button">
+                        {{-- ★ buttonに 'icon-liked' クラスを追加 --}}
+                        <button type="submit" class="like-button icon-liked">
                             <i class="fas fa-heart"></i>
                         </button>
                     </form>
                 @else
+                    {{-- まだいいねしていない場合（いいねフォーム） --}}
                     <form action="{{ route('posts.like', $post) }}" method="POST" class="like-form"
                         data-post-id="{{ $post->id }}" style="display: inline;">
                         @csrf
+                        {{-- ★ こちらのbuttonにはクラスは不要 --}}
                         <button type="submit" class="like-button">
                             <i class="far fa-heart"></i>
                         </button>
@@ -32,19 +37,32 @@
                 @endif
             </div>
 
-            {{-- いいね機能のすぐ隣に配置 --}}
             <button type="button" class="comment-toggle-button" data-post-id="{{ $post->id }}">
                 <i class="far fa-comment" style="font-size: 24px; color: #333;"></i>
             </button>
         </div>
         <span id="bookmark-section-{{ $post->id }}">
-            <form action="{{ route('posts.unbookmark', $post) }}" method="POST" class="bookmark-form"
-                data-post-id="{{ $post->id }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="bookmark-button">
-                    <i class="fas fa-bookmark icon-bookmarked"></i> </button>
-            </form>
+            {{-- ★ ブックマークの状態に応じてフォームとアイコンを切り替える --}}
+            @if (Auth::check() && $post->isBookmarkedBy(Auth::user()))
+                {{-- ブックマーク済みの場合（解除フォーム） --}}
+                <form action="{{ route('posts.unbookmark', $post) }}" method="POST" class="bookmark-form"
+                    data-post-id="{{ $post->id }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="bookmark-button icon-bookmarked">
+                        <i class="fas fa-bookmark"></i>
+                    </button>
+                </form>
+            @else
+                {{-- まだブックマークしていない場合（登録フォーム） --}}
+                <form action="{{ route('posts.bookmark', $post) }}" method="POST" class="bookmark-form"
+                    data-post-id="{{ $post->id }}">
+                    @csrf
+                    <button type="submit" class="bookmark-button">
+                        <i class="far fa-bookmark"></i>
+                    </button>
+                </form>
+            @endif
         </span>
     </div>
     <div class="comments-container" id="comments-container-{{ $post->id }}"
