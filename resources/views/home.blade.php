bd9d42b781cccb94e661880cb48790c69e4859d2
nemoto <haikonggenben@gmail.com>
2025-09-18 12:20:43 +0900

    repair
    syuusei

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,13 +24,13 @@
             <div class="post-header">
                 <p class="post-author">
                     {{ $post->user ? '@' . ($post->user->username ?? $post->user->name) : '@unknown' }}
-                </p>
-            </div>
+                    </p>
+                </div>
             <div class="post-image-container">
                 @if ($post->img_url)
                     <img src="{{ asset('storage/' . $post->img_url) }}" alt="Post Image">
                 @endif
-            </div>
+                </div>
             <div class="post-actions">
                 <div class="actions-left" style="display: flex; align-items: center; gap: 50px;">
                     <div class="like-section" id="like-section-{{ $post->id }}">
@@ -40,34 +40,36 @@
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="like-button">
-                                    <i class="fas fa-heart" ></i>
-                                </button>
-                            </form>
+                                    <i class="fas fa-heart"></i>
+                                    </button>
+                                </form>
                         @else
                             <form action="{{ route('posts.like', $post) }}" method="POST" class="like-form"
                                 data-post-id="{{ $post->id }}" style="display: inline;">
                                 @csrf
                                 <button type="submit" class="like-button">
                                     <i class="far fa-heart"></i>
-                                </button>
-                            </form>
+                                    </button>
+                                </form>
                         @endif
-                    </div>
+                        </div>
 
                     {{-- いいね機能のすぐ隣に配置 --}}
-                    <button type="button" class="comment-toggle-button" data-post-id="{{ $post->id }}">
+                    <button type="button" class="comment-toggle-button"
+                        data-post-id="{{ $post->id }}">
                         <i class="far fa-comment" style="font-size: 24px; color: #333;"></i>
-                    </button>
-                </div>
+                        </button>
+                    </div>
                 <span id="bookmark-section-{{ $post->id }}">
-    <form action="{{ route('posts.unbookmark', $post) }}" method="POST" class="bookmark-form" data-post-id="{{ $post->id }}">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="bookmark-button">
-            <i class="fas fa-bookmark icon-bookmarked"></i> </button>
-    </form>
-</span>
-            </div>
+                    <form action="{{ route('posts.unbookmark', $post) }}" method="POST"
+                        class="bookmark-form"                         data-post-id="{{ $post->id }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bookmark-button">
+                            <i class="fas fa-bookmark icon-bookmarked"></i> </button>
+                        </form>
+                    </span>
+                </div>
             <div class="comments-container" id="comments-container-{{ $post->id }}"
                 style="display: none; margin-top: 10px;">
 
@@ -75,38 +77,38 @@
                     @foreach ($post->comments as $comment)
                         <div
                             class="comment-item
-                @if ($comment->user_id === Auth::id()) my-comment @else other-comment @endif">
+                @if ($comment->user_id === Auth::id()) my-comment @else other-comment @endif">
                             <strong>{{ $comment->user->name }}</strong>
                             <span>{{ $comment->content }}</span>
-                        </div>
+                            </div>
                     @endforeach
-                </div>
+                    </div>
 
                 {{-- コメント投稿フォーム --}}
                 <form action="{{ route('comments.store', $post) }}" method="POST" class="new-comment-form"
                     data-post-id="{{ $post->id }}">
                     @csrf
-                    <input type="text" name="content" class="comment-input" placeholder="コメントを追加..." required>
+                    <input type="text" name="content" class="comment-input" placeholder="コメントを追加..."
+                        required>
                     <button type="submit" class="comment-submit-button">投稿</button>
-                </form>
-            </div>
+                    </form>
+                </div>
             <div class="post-content">
                 @if ($post->tags->isNotEmpty())
                     <div class="post-tags">
                         @foreach ($post->tags as $tag)
                             <span class="tag">{{ $tag->name }}</span>
                         @endforeach
-                    </div>
+                        </div>
                 @else
                     <div class="post-tags">
                         <span class="tag">タグなし</span>
-                    </div>
+                        </div>
                 @endif
 
                 <p class="caption">{{ $post->text }}</p>
-            </div>
-        </article>
-        @include('posts._post_card', ['post' => $post])
+                </div>
+            </article>
     @endforeach
 @endsection
 @section('scripts')
